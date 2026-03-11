7dd808948143d3177168958550b9326263f65665
nagendradot2 <nagendrashiva966@gmail.com>
2026-03-11 17:57:52 +0000

    This is to add create user with expiry

---
+++ expiry.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###########################
+# Author: Nagendra
+# Date: 08-03-2026
+# Version: V2
+###########################
+
+set -o pipefail
+set -e
+set -x
+
+# Check if arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <username> <expiry-date>"
+    echo "Example: $0 siva 2026-12-31"
+    exit 1
+fi
+
+user=$1
+expiry_date=$2
+
+# Create user with expiry date
+sudo useradd -e "$expiry_date" "$user"
+
+# Set password
+sudo passwd "$user"
+
+# Check user expiry details
+sudo chage -l "$user"
